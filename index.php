d70db4278f4d345f43c90eadcfb437be8b1699a1
= <=>
2021-10-25 15:24:17 +0700

    fix responsive header

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,22 +8,34 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
     <style>
-         @media screen and (min-width: 1100px)
-         {
+        @media screen and (min-width: 1024px) {
             .nav-logout1 {
-            display: none;
+                display: none;
+            }
         }
-         }
-        @media screen and (max-width: 900px) and (min-width: 600px) {
-        .drop-logout {
-            display: none;
+
+        @media screen and (min-width: 740px) and (max-width: 1023px) {
+            .drop-logout {
+                display: none;
+            }
+
+            .nav-logout1 {
+                display: block;
+            }
         }
-        .nav-logout1 {
-            display: block;
-        }
+
+        @media screen and (max-width:740px) {
+            .drop-logout {
+                display: none;
+            }
+
+            .nav-logout1 {
+                display: block;
+            }
         }
     </style>
 </head>
+
 <body>
     <div class="warpper">
         <div class="container-fluid">
@@ -30,47 +43,50 @@
                 <div class="col-md-12 p-0">
                     <nav class="navbar navbar-expand-lg navbar-light bg-light">
                         <div class="container">
-                            
+
                             <a class="navbar-brand" href="#">Navbar scroll</a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                                <span class="navbar-toggler-icon"></span>
                             </button>
                             <div class="collapse navbar-collapse" id="navbarScroll">
-                            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-                                <li class="nav-item">
-                                     <a class="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li class="nav-item">
-                                     <a class="nav-link" href="#">Link</a>
-                                </li>
-                                <li class="nav-item  nav-logout1" >
-                                    <a class="nav-link" href="#" tabindex="-1" >Sign out</a>
-                                </li>
-                            </ul>
-                            <form class="d-flex">
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                               
-                            
-                            </form>
-                             <div class="dropdown text-end mr-4 drop-logout">
-                                <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
-                                </a>
-                                <ul class="dropdown-menu  " style=" min-width: 8rem;"  aria-labelledby="dropdownUser1">
-                                    <li><a class="dropdown-item" href="#">New project...</a></li>
-                                    <li><a class="dropdown-item" href="#">Settings</a></li>
-                                    <li><a class="dropdown-item" href="#">Profile</a></li>
-                                    <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="#">Sign out</a></li>
+                                <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Link</a>
+                                    </li>
+                                    <li class="nav-item  nav-logout1">
+                                        <a class="nav-link" href="#" tabindex="-1">Sign out</a>
+                                    </li>
                                 </ul>
-                            </div>
+                                <form class="d-flex">
+                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+
+
+                                </form>
+                                <div class="dropdown text-end mr-4 drop-logout">
+                                    <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                                    </a>
+                                    <ul class="dropdown-menu  " style=" min-width: 8rem;" aria-labelledby="dropdownUser1">
+                                        <li><a class="dropdown-item" href="#">New project...</a></li>
+                                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </nav>
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+
 </html>

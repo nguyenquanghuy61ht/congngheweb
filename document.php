b608163151a2a7899591e650836d34cc5451baab
Đào Minh Quyền <daoquyenminh@gmail.com>
2021-10-27 16:02:06 +0700

    update 27-10

--- document.php
+++ document.php
@@ -1,6 +1,52 @@
 <h3 class="mb-3 text-primary">Tài liệu tham khảo</h3>
 <div class="col-md-10 border-bottom mb-3">
-    link1
+    <ol class="list-group list-group-numbered">
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+                <div class="fw-bold">Tiếng Anh</div>
+                Compact PET For Schools Teacher's book 
+            </div>
+            <a href="" class="badge bg-success rounded-pill">Tải Xuống</a>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+                <div class="fw-bold">Tiếng Anh</div>
+                Speak English like an American
+            </div>
+            <a href="" class="badge bg-success rounded-pill">Tải Xuống</a>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+                <div class="fw-bold">Tiếng Anh</div>
+                Business Goals Professional English 
+            </div>
+            <a href="" class="badge bg-success rounded-pill">Tải Xuống</a>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+                <div class="fw-bold">Tiếng Anh</div>
+                English Vocabulary in use 
+            </div>
+            <a href="" class="badge bg-success rounded-pill">Tải Xuống</a>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+                <div class="fw-bold">Tiếng Anh</div>
+                Compact PET For Schools Teacher's book 
+            </div>
+            <a href="" class="badge bg-success rounded-pill">Tải Xuống</a>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+                <div class="fw-bold">Tiếng Anh</div>
+                Compact PET For Schools Teacher's book 
+            </div>
+            <a href="" class="badge bg-success rounded-pill">Tải Xuống</a>
+        </li>
+        
+    </ol>
+
+
 </div>
 <div class="col-md-10 border-bottom mb-3">
     link2

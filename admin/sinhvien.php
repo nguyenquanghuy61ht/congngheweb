989ea9b99353052e021dda1cecd06f06111e7005
NguyenQuangHuy61HT <huyn67811@gmail.com>
2021-10-29 20:23:33 +0700

    fix giao dien

--- admin/sinhvien.php
+++ admin/sinhvien.php
@@ -1,25 +1,31 @@
-  <h5 class="text-success">Quản lý Sinh viên</h5>
-  <table class="table table-bordered mt-3 ">
-    <thead class="thead-dark ">
-      <tr>
-        <th scope="col">STT</th>
-        <th scope="col">Tên sinh viên</th>
-        <th scope="col">Lớp</th>
-        <th scope="col">Số điện thoại </th>
-        <th scope="col">email</th>
-        <th scope="col">Ngày đăng ký</th>
-        <th scope="col">Trạng thái</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Nguyễn Quang Huy</td>
-        <td>61HT</td>
-        <td>098983982</td>
-        <td>huy23@gmail.com</td>
-        <td>15-4-2020</td>
-        <td>1</td>
+ <div class="col-md-12">
+   <h5 class="text-success">Quản lý Sinh viên</h5>
+ </div>
 
-      </tr>
+ <div class="col-md-12">
+   <table class="table table-bordered mt-3 ">
+     <thead class="thead-dark ">
+       <tr>
+         <th scope="col">STT</th>
+         <th scope="col">Tên sinh viên</th>
+         <th scope="col">Lớp</th>
+         <th scope="col">Số điện thoại </th>
+         <th scope="col">email</th>
+         <th scope="col">Ngày đăng ký</th>
+         <th scope="col">Trạng thái</th>
+       </tr>
+     </thead>
+     <tbody>
+       <tr>
+         <th scope="row">1</th>
+         <td>Nguyễn Quang Huy</td>
+         <td>61HT</td>
+         <td>098983982</td>
+         <td>huy23@gmail.com</td>
+         <td>15-4-2020</td>
+         <td>1</td>
 
+       </tr>
+     </tbody>
+   </table>
+ </div>

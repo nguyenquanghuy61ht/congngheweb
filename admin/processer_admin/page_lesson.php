cbff72b727ac22b53837e9b371541e022fb285fc
phucvandeptai1 <nguyenvanphuc1912001@gmail.com>
2021-10-30 16:32:23 +0700

    chat

--- admin/processer_admin/page_lesson.php
+++ admin/processer_admin/page_lesson.php
@@ -8,9 +8,42 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/processer.css">
 </head>
 
 <body>
+    <div class="contener-fluid">
+        <div class="contener">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light  ">
+                                            <a class="navbar-brand" href="#">Navbar</a>
+                                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                                <span class="navbar-toggler-icon"></span>
+                                            </button>
+                                            <div class="collapse navbar-collapse" id="navbarNav">
+                                                <ul class="navbar-nav">
+                                                <li class="nav-item active">
+                                                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Features</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="#">Pricing</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link disabled" href="#">Disabled</a>
+                                                </li>
+                                                </ul>
+                                            </div>
+                            </nav>
+
+                </div>
+            </div>
+        </div>
+        
+    </div>
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">

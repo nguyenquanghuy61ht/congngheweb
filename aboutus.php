e76d3def1af3c6789eb109e16caaa8e4979f939f
Đào Minh Quyền <daoquyenminh@gmail.com>
2021-11-03 20:40:20 +0700

    change about us

--- aboutus.php
+++ aboutus.php
@@ -1,22 +1,8 @@
-<?php
-include('login_check.php')
-?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/responsive.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>Học Mãi Mãi </title>
 
-</head>
-<body>
-    <?php include "./page-subject/header_lesson.php" ?>
+    <?php include "./partials/header.php" ?>
+
+
      <div class="container">
          <div class="row">
              <div class="col-md-12">
@@ -74,8 +60,7 @@ include('login_check.php')
 
 
     <?php include "./partials/footer.php" ?>
-</body>
-<html>
+
 
 
 

c7422534bb6c79368202b712b0b63b99491da77f
phucvandeptai1 <nguyenvanphuc1912001@gmail.com>
2021-11-03 21:53:25 +0700

    question

--- question.php
+++ question.php
@@ -1,1 +1,66 @@
-1
+<div class="panel panel-default">
+  <div class="panel-body">bài giảng nào tốt nhất </div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">ai là ceo của đội ngũ phát triển web </div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">ai là người code chính của dự án </div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">A Basic Panel</div>
+</div>
